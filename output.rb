32c7d2957a17b36b0c069b22a9f7dc8e4b904c3e
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-09-14 13:04:31 +0200

    Добавил файл output.rb для соотв. класса. Ни какой логики не содержит. Пока только коментарии.

---
+++ output.rb
@@ -0,0 +1,25 @@
+  #require 'roo'
+  #require_realtive where NAME input parameter is set
+    # NAME = ''
+
+class Output
+  #include 'roo'
+
+  #def initialize ("smth")
+    #set @instance_variable to "smth"
+ #end
+
+  #def file_opener method (name)
+
+    #This is how to make file name for xlsx file:
+      #@template_folder = File.join(Dir.home, NAME)
+      #@template_file = File.join(@template_folder, "export_template_#{Time.now.round(3).to_s.chomp(' -0700').gsub(':','-').gsub(' ','_').chop.chop.chop}.xlsx")
+
+    #Open file
+
+  #end
+
+  # def appender ("row","column", "data")
+    #Roo.SomeInstance("row","column", "data", nil)
+  #end
+end
