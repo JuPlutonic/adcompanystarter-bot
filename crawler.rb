0f0ae5ec0fb0fd1d329c666fb3d9eea8f3813eee
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-09-15 15:31:49 +0200

    Дописал комментарии.

---
+++ crawler.rb
@@ -0,0 +1,32 @@
+require 'rubygems' # Bundle gems
+require 'mechanize' # Check for relevant forms and buttons and manipulate
+require 'nokogiri' # Web parser
+require 'open-uri' # CSS selectors
+require_relative 'output.rb' # To use in #collect method: xlsx/csv export
+
+#PAGE_URL = "https://target-sandbox.my.com/ads/campaigns/new" #Development
+#Production is PAGE_URL = "https://target.my.com/ads/campaigns/new"
+
+#ARGV0 login
+#ARGV1 password
+#ARGV2 mobile_site
+#gets name
+
+Class Crawler
+
+  #def sign_in (login, password)
+  #end
+
+  def company_start
+  end
+
+  public
+
+  #def collect
+
+    #app_id
+    #placement # array
+    #ad_unit # array
+
+  #end
+end
--- output.rb
+++ output.rb
@@ -1,25 +1,25 @@
   #require 'roo'
-  #require_realtive where NAME input parameter is set
+  #require_realtive # Where NAME input parameter is set
     # NAME = ''
 
 class Output
   #include 'roo'
 
   #def initialize ("smth")
-    #set @instance_variable to "smth"
- #end
+    # Set @instance_variable to "smth"
+  #end
 
   #def file_opener method (name)
 
-    #This is how to make file name for xlsx file:
+    # This is how to make file name for xlsx/csv file:
       #@template_folder = File.join(Dir.home, NAME)
       #@template_file = File.join(@template_folder, "export_template_#{Time.now.round(3).to_s.chomp(' -0700').gsub(':','-').gsub(' ','_').chop.chop.chop}.xlsx")
 
-    #Open file
+    # Open file
 
   #end
 
-  # def appender ("row","column", "data")
-    #Roo.SomeInstance("row","column", "data", nil)
+  #def appender ("row","column", "data")
+    # Something like Update commentsRoo.SomeInstance("row","column", "data", nil)
   #end
 end
