b6e9734fc099edae418cd61e1f4910c36be4ca98
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-09-16 13:45:53 +0200

    Убрал примеры не для DSL Cucumber и RSpec.

--- spec/adstarter_app_spec.rb
+++ spec/adstarter_app_spec.rb
@@ -1,21 +1,3 @@
-# # When you use (require) gem 'shoulda' with these matchers you can make many tricks:
-# it { should validate_presence_of :name }
-# it { should validate_presence_of :url }
-# it { should validate_presence_of :platform_id }
-# it { should validate_presence_of :block_types }
-# it { should validate_numericality_of(:platform_id).only_integer.is_greater_than(0)
-
-# # When you use mechanize there are methods testcase module:
-# require 'mechanize/test_case'
-# class RedirectLimitReachedError < Mechanize::TestCase
-#   def test_to_s
-#     app_host = 'https://target-sandbox.my.com/ads/campaigns/new' # Development
-#     # For work in production please set app_host = "https://target.my.com/ads/campaigns/new"
-#     error = Mechanize::ElementNotFoundError.new(app_host, :element, :conditions)
-#     assert_match(/Campaigns/, error.to_s)
-#   end
-# end
-
 # # When you use capybara, poltergeist and phantom.js testing is pretty easy:
 # Capybara.default_driver = :poltergeist
 # Capybara.app_host = 'https://target-sandbox.my.com/ads/campaigns/new' # Development
