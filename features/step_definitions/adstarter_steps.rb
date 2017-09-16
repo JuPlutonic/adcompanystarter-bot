d1e9ba04841f01c306a9f561e549742839dfdf2a
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-09-16 23:00:14 +0200

    .

--- features/step_definitions/adstarter_steps.rb
+++ features/step_definitions/adstarter_steps.rb
@@ -5,18 +5,19 @@ require 'capybara/dsl'
 require 'rspec/expectations'
 include RSpec::Matchers
 
-Given /^I am on the Target Sandbox Page$/ do
-  visit "http://target-sandbox.my.com"
-end
-
-Then /^I should see "([^myTarget]*)"$/ do |text|
-  page.have_content? text
+Given /^I am on the target page$/ do
+  visit "https://target-sandbox.my.com"
 end
 
 Then /^I should see "([^"]*)" in the selector "([^"]*)"$/ do |text, selector|
-  page.should have_selector selector, content: text
+  page.should have_selector(selector, text: text)
 end
 
-Then /^I should see "([^"]*)" in a link$/ do |text|
-  page.should have_link text
+When /^I click "([^"]*)"$/ do |element|
+  find(:xpath, element).click
 end
+
+Then /^I also should see "([^"]*)" in the selector "([^"]*)"$/ do |text, selector|
+  page.should have_selector(selector, text: text)
+end
+
