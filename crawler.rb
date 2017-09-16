0614d2cb0f74e9249084e42999322ee37303096b
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-09-16 13:28:00 +0200

    Добавил/исправил коментарии в файле crawler.rb.

--- crawler.rb
+++ crawler.rb
@@ -1,18 +1,18 @@
 require 'rubygems' # Bundle gems
 require 'mechanize' # Check for relevant forms and buttons and manipulate
-require 'nokogiri' # Web parser
-require 'open-uri' # CSS selectors
+require 'nokogiri' # Web parser. CSS/XPath selectors.
+require 'open-uri'
 require_relative 'output.rb' # To use in #collect method: xlsx/csv export
 
 #PAGE_URL = "https://target-sandbox.my.com/ads/campaigns/new" #Development
 #Production is PAGE_URL = "https://target.my.com/ads/campaigns/new"
 
-#ARGV0 login
-#ARGV1 password
-#ARGV2 mobile_site
-#gets name
+# Use ARGV0 for login
+# Use ARGV1 for password
+# Use ARGV2 for mobile_site
+# And gets for name
 
-Class Crawler
+class Crawler
 
   #def sign_in (login, password)
   #end
@@ -24,9 +24,9 @@ Class Crawler
 
   #def collect
 
-    #app_id
-    #placement # array
-    #ad_unit # array
+    # Saves app_id
+    # Collects placement # array
+    # Collects ad_unit # array
 
   #end
 end
