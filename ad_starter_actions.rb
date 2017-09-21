fd6c3f6e91308954ef559c6ddea98c1ee20ca5dc
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-09-21 20:32:02 +0200

    Рефакторинг: файл ad_starter_actions.rb метод validate_page_title.

--- ad_starter_actions.rb
+++ ad_starter_actions.rb
@@ -6,24 +6,23 @@
 
 # Pull in the action title that the Script refers to.
 def test_task_title
-  puts @originalActionTitle # ad_starter_properties
+  puts @original_action_title # ad_starter_properties
 end # test_title
 
 # Fetch the page URL to use and open it.
 def test_page_url
-  @driver.get @testUrl # ad_starter_properties
+  @driver.get @test_Url # ad_starter_properties
 end # get_page_url
 
 # Check the web page title is as expected.
 def validate_page_title
-  if @driver.title != @expectedResult
+  if @driver.title != @expected_result
     failedTest # ad_starter_global_methods
-    fail_message = "[FAIL] Title was #{@driver.title}, but we expected #{@expectedResult}"
-    puts fail_message
-    @testLog.puts("#{@originalActionTitle} \n #{fail_message}")
+    validation_message =
+      "[FAIL] Title was #{@driver.title}, but we expected #{@expected_result}"
   else
-    pass_message = '[PASS] Title was as expected'
-    puts pass_message
-    @testLog.puts("#{@originalActionTitle} \n #{pass_message}")
+    validation_message = '[PASS] Title was as expected'
   end
+  puts validation_message
+  @testLog.puts("#{@original_action_title} \n #{validation_message}")
 end
