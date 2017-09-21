97d4a811ca4a1f89ade75c591a41f029c41f3b72
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-09-21 20:13:04 +0200

    output.rb rubocoped.

--- output.rb
+++ output.rb
@@ -2,14 +2,14 @@
 
 # Methods in this file are divided by easily distinguishing tasks.
 # So program is easy to test.
-# --------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 def visit_site_task
-  test_tasks_title           # ad_starter_actions.rb
-  setup                     # ad_starter_global_methods.rb
-
-    get_page_url            # ad_starter_actions.rb
-    validate_page_title     # ad_starter_actions.rb
-
-  teardown                  # ad_starter_global_methods.rb
+  test_tasks_title # ad_starter_actions.rb
+  setup # ad_starter_global_methods.rb
+  # setup Selenium
+  test_page_url # ad_starter_actions.rb
+  validate_page_title # ad_starter_actions.rb
+  # teardown Selenium
+  teardown # ad_starter_global_methods.rb
 end
