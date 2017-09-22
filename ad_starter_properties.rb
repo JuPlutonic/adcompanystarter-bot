d9fa9d48560419c2cd8154b51e60f7a5c3efafdb
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-09-22 13:02:49 +0200

    Изменили способ вызова программы. "TARG_LOGIN=john@mail.io TARG_PWD=12345678 ruby crawler.rb"

--- ad_starter_properties.rb
+++ ad_starter_properties.rb
@@ -6,3 +6,7 @@
 @test_url = 'https://target-sandbox.my.com/'
 @expected_result = 'Target.my.com' # <-- Change this to BREAK the test
 @web_driver_to_use = :chrome
+# Example.
+# How to run script with login and password: TARG_LOGIN=john@mail.io TARG_PWD=12345678 ruby crawler.rb
+@env_login = ENV['TARG_LOGIN'] .to_s || 'error' # Set it before run crawler.rb
+@env_password = ENV['TARG_PWD'].to_s || 'error' # Set it before run crawler.rb
